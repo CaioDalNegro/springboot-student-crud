8f645f46a96332dfba4c812954c7efd204e0ac78
CaioDalNegro <caiodalnegro@gmail.com>
2025-08-26 21:55:22 -0300

    Estrutura do Spring

---
+++ src/main/java/br/com/alunocrud/springboot_student_crud/SpringbootStudentCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.alunocrud.springboot_student_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alunocrud/springboot_student_crud/SpringbootStudentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alunocrud.springboot_student_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
